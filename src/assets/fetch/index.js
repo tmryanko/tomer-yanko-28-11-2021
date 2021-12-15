b53f76c123b00081af063ddc40e4c5c030b49d92
Tomer Yanko <tmryanko@gmail.com>
2021-12-15 10:14:18 +0200

    s

--- src/assets/fetch/index.js
+++ src/assets/fetch/index.js
@@ -1,5 +1,5 @@
 export const ApiCall = async (link, body) => {
-  const WEATHER_LINK = 'http://dataservice.accuweather.com';
+  const WEATHER_LINK = 'https://dataservice.accuweather.com';
   const res = await fetch(`${WEATHER_LINK}${link}`);
   const json = await res.json();
 
